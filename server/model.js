1b2571f2c16659e44a61215045bb9f83352ba13e
mmcphillips <matthewcmcphillips@gmail.com>
2019-07-11 00:50:05 -0700

    Implement model for pulling image urls from the database

--- server/model.js
+++ server/model.js
@@ -1,16 +1,16 @@
-const database = require('./database.js');
+const DatabaseConnection = require('./database.js');
 
-const getImages = function(params){
-  var query = "select * from images WHERE id = ?;";
-  return new Promise( (resolve, reject) => {
-    database.query(query, params, function(err,results){
-      if (err){
+const getImages = function (params) {
+  const query = 'select * from images WHERE id = ?;';
+  return new Promise((resolve, reject) => {
+    DatabaseConnection.query(query, params, (err, results) => {
+      if (err) {
         reject(err);
-      } else{
+      } else {
         resolve(results);
       }
-    })
-  })
-}
+    });
+  });
+};
 
 module.exports = getImages;
